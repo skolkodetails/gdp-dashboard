80a2cc124ff1cdca8c955557183de8183097e14b
skolkodetails <vera.meshchenko.23.08@gmail.com>
2026-03-31 21:15:53 +0000

    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,82 @@
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+import plotly.express as px
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
-
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
-
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+st.set_page_config(page_title = "titanic dashboard", page_icon = "🚢oops", layout = "wide")
+st.title("🚢 dashboard: titanic passengers")
+st.markdown("This dashboard is created for interactive analysis of data about the passengers of the legendary Titanic "
+ "Here we can see statistics, passenger distribution, and factors that influenced survival")
+@st.cache_data #используем кэш для быстрой работы приложения
+def load_data():
+    url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+    return pd.read_csv(url)
+df = load_data()
+st.divider() #горизонтальная линия
+# вывод энного количества строк
+st.header("1. Viewing raw data")
+n_rows = st.slider("Select the number of lines to display:", min_value = 1, max_value = len(df), value = 5)
+st.dataframe(df.head(n_rows))
+st.divider()
+# описательная статистика 
+st.header("2. Descriptive statistics")
+st.markdown("This section provides basic information about the format of our table and the types of data")
+# разбиваем экран на 2 колонки для вывода метрик
+col1, col2 = st.columns(2)
+with col1:
+    st.metric("Number of passengers (rows)", df.shape[0])
+with col2:
+    st.metric("Number of features (columns)", df.shape[1])
+# вывод типов данных и названий столбцов
+st.subheader("Columns and data types:")
+# Преобразуем dtypes в красивую табличку
+dtypes_df = pd.DataFrame(df.dtypes, columns = ['Тип данных']).astype(str)
+st.dataframe(dtypes_df, use_container_width = True)
+st.divider()
+st.header("3. Data visualization")
+# создаем две колонки чтобы разместить графики рядом
+chart_col1, chart_col2 = st.columns(2)
+with chart_col1:
+    # Pie Chart
+    st.subheader("Survival status")
+    # заменим 0 и 1 на текст для понятности
+    surv_counts = df['Survived'].map({0: 'dead', 1: 'survived'}).value_counts().reset_index()
+    surv_counts.columns = ['status', 'amount']
+    fig1 = px.pie(surv_counts, names = 'status', values = 'amount', color = 'status', 
+                  color_discrete_map = {'dead': '#EF553B', 'survived': '#00CC96'})
+    st.plotly_chart(fig1, use_container_width=True)
+    # Bar Chart
+    st.subheader("Passengers by ticket class")
+    pclass_counts = df['Pclass'].value_counts().reset_index()
+    pclass_counts.columns = ['Class (1-elite, 3-economy)', 'amount']
+    # Делаем класс строкой, чтобы он не воспринимался как непрерывное число на графике
+    pclass_counts['Class (1-elite, 3-economy)', 'Quantity'] = pclass_counts['Class (1-elite, 3-economy)'].astype(str)
+    fig2 = px.bar(pclass_counts, x = 'Class (1-elite, 3-economy)', y = 'amount', color = 'Class (1-elite, 3-economy)')
+    st.plotly_chart(fig2, use_container_width=True)
+with chart_col2:
+    # Histogram
+    st.subheader("Age distribution")
+    fig3 = px.histogram(df, x = "Age", nbins=20, labels={"Age": "age"}, color_discrete_sequence = ['#636EFA'])
+    st.plotly_chart(fig3, use_container_width = True)
+    #Scatter Plot
+    st.subheader("Dependence: Age and Ticket Price")
+    df_scatter = df.copy()
+    df_scatter['status'] = df_scatter['Survived'].map({0: 'died', 1: 'survived'})
+    fig4 = px.scatter(df_scatter, x = "Age", y = "Fare", color = "status", 
+                      labels = {"Age": "age", "Fare": "price"})
+    st.plotly_chart(fig4, use_container_width = True)
+st.divider() 
+# график реагирующий на ввод пользователя
+st.header("4. Interactive survival analysis")
+st.markdown("Select a parameter from the drop-down list below to see what percentage of passengers survived based on the selected feature")
+selected_column = st.selectbox(
+    "Select a parameter:",
+    ("Sex", "Pclass", "Embarked"))
+column_names = {"Sex": "Sex", "Pclass": "Pclass", "Embarked": "Embarked"}
+survival_rates = df.groupby(selected_column)['Survived'].mean().reset_index()
+survival_rates['Survived'] = survival_rates['Survived'] * 100 
+# интерактивный столбчатый график
+st.subheader(f"Percentage of survivors based on: {column_names[selected_column]}")
+fig5 = px.bar(survival_rates, x=selected_column, y='Survived',
+              labels = {selected_column: column_names[selected_column], 'Survived' : 'survived %'},
+              color = selected_column, text_auto = '.1f') #цифры на столбики
+st.plotly_chart(fig5, use_container_width = True)
